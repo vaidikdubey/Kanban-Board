95dfeb64e02ee237959e843df8855caa0b7616ec
vaidikdubey <vaidik26dubey@gmail.com>
2025-05-21 22:50:05 +0530

    Added edit and delete buttons. Added mode change for boards and buttons

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const modeButton = document.getElementById("modeButton");
-const board = document.getElementsByClassName('.board');
+const board = document.querySelectorAll(".board");
+const editDeleteBtn = document.querySelectorAll(".edit-delete-btn");
 
 modeButton.addEventListener("click", changeMode);
 
@@ -10,12 +11,31 @@ function changeMode() {
     document.body.style.backgroundColor = "#ffffff";
     document.body.style.color = "#1e1e1e";
     modeButton.textContent = "Dark 🌚";
-}
-else {
+    board.forEach((boards) => {
+      boards.style.color = "#1e1e1e";
+      boards.style.backgroundColor = "#bbbbbb";
+    });
+    editDeleteBtn.forEach((button) => {
+      button.classList.remove("btn-outline-light");
+      button.classList.add("btn-outline-dark");
+    });
+  } else {
     modeButton.classList.remove("btn-dark");
     modeButton.classList.add("btn-light");
     document.body.style.backgroundColor = "#1e1e1e";
     document.body.style.color = "#ffffff";
     modeButton.textContent = "Light 🌞";
+    board.forEach((boards) => {
+      boards.style.color = "#ffffff";
+      boards.style.backgroundColor = "#3e3e3e";
+    });
+    editDeleteBtn.forEach((button) => {
+      button.classList.remove("btn-outline-dark");
+      button.classList.add("btn-outline-light");
+    });
   }
 }
+
+const pendingList = document.getElementById("pendingList");
+const progressList = document.getElementById("progressList");
+const completedList = document.getElementById("completedList");
